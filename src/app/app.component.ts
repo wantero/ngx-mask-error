da39ba85d201c84557b142dd922fe773a27be749
wantero <wantero@dlctec.com.br>
2019-04-12 11:01:46 -0300

    ngx-mask-error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ngx-mask-error';
+export class AppComponent implements OnInit {
+
+  formGroup: FormGroup;
+
+  constructor(
+    private _formBuilder: FormBuilder
+  ) {
+    this.formGroup = _formBuilder.group({
+      phone: ['', [Validators.required, Validators.pattern('^\\([0-9]{2}\\) [0-9]{4,5}-[0-9]{4}$')]],
+      postalCode: ['', [Validators.required, Validators.pattern('^\\d{5}[-]\\d{3}$')]],
+      phone2: ['', [Validators.required, Validators.pattern('^\\([0-9]{2}\\) [0-9]{4,5}-[0-9]{4}$')]],
+      postalCode2: ['', [Validators.required, Validators.pattern('^\\d{5}[-]\\d{3}$')]]
+    });
+  }
+
+  ngOnInit() {
+  }
+
 }
